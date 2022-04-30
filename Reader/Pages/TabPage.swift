a521cbbc9a98b5306bff3f91946d4db0dd90b178
Jayden Liu <67111677@qq.com>
2022-04-30 14:02:57 +0800

    update navigation bar's 'isHidden' property by tab bar index

--- Reader/Pages/TabPage.swift
+++ Reader/Pages/TabPage.swift
@@ -9,31 +9,45 @@ import SwiftUI
 import Introspect
 
 struct TabPage: View {
-    let navigationHandler = NavigationControllerHandler()
+    @State private var tabBarIndex = NavigationCoordinator.shared.tabBarIndex
     
-    @State private var tabBarIndex = 1
-    let tabItemInfos = [
-        TabItemInfo(title: "书架", imageName: "tab_bookshelf_n", selectedImageName: "tab_bookshelf_p"),
-        TabItemInfo(title: "书城", imageName: "tab_bookstore_n", selectedImageName: "tab_bookstore_p"),
-        TabItemInfo(title: "我的", imageName: "tab_me_n", selectedImageName: "tab_me_p")
+    static let tabItemInfos = [
+        TabItemInfo(title: "书架",
+                    imageName: "tab_bookshelf_n",
+                    selectedImageName: "tab_bookshelf_p",
+                    navigationBarHidden: true),
+        TabItemInfo(title: "书城",
+                    imageName: "tab_bookstore_n",
+                    selectedImageName: "tab_bookstore_p",
+                    navigationBarHidden: true),
+        TabItemInfo(title: "我的",
+                    imageName: "tab_me_n",
+                    selectedImageName: "tab_me_p",
+                    navigationBarHidden: true)
     ]
     
     var body: some View {
         NavigationView() {
             TabView(selection: $tabBarIndex) {
-                BookshelfPage(viewModel: BookshelfViewModel()).tabItem { createTabItem(tabItemInfos[0]) }.tag(0)
-                BookstorePage().tabItem { createTabItem(tabItemInfos[1]) }.tag(1)
-                MePage().tabItem { createTabItem(tabItemInfos[2]) }.tag(2)
+                BookshelfPage(viewModel: BookshelfViewModel()).tabItem { createTabItem(TabPage.tabItemInfos[0]) }.tag(0)
+                BookstorePage().tabItem { createTabItem(TabPage.tabItemInfos[1]) }.tag(1)
+                MePage().tabItem { createTabItem(TabPage.tabItemInfos[2]) }.tag(2)
             }.navigationBarTitleDisplayMode(.inline).introspectNavigationController { (navigationController) in
                 navigationController.navigationBar.isHidden = true
-                navigationController.delegate = navigationHandler
+                navigationController.delegate = NavigationCoordinator.shared
+                NavigationCoordinator.shared.mainNavigationController = navigationController
+            }.onChange(of: tabBarIndex) { newValue in
+                print("On tap tab bar item \(newValue)")
+                NavigationCoordinator.shared.tabBarIndex = newValue
             }
+        }.onAppear {
+            
         }
     }
     
     @ViewBuilder
     func createTabItem(_ info: TabItemInfo) -> some View {
-        let isSelected = tabItemInfos.firstIndex { $0 == info } == tabBarIndex
+        let isSelected = TabPage.tabItemInfos.firstIndex { $0 == info } == tabBarIndex
         if isSelected {
             Image(info.selectedImageName)
         } else {
@@ -47,14 +61,25 @@ struct TabItemInfo: Equatable {
     let title: String
     let imageName: String
     let selectedImageName: String
+    let navigationBarHidden: Bool
 }
 
-class NavigationControllerHandler: NSObject, UINavigationControllerDelegate {
+class NavigationCoordinator: NSObject, UINavigationControllerDelegate {
+    static let shared = NavigationCoordinator()
+    
+    weak var mainNavigationController: UINavigationController?
+    
+    var tabBarIndex = 1 {
+        didSet {
+            mainNavigationController?.navigationBar.isHidden = TabPage.tabItemInfos[tabBarIndex].navigationBarHidden
+        }
+    }
+    
     func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
         viewController.hidesBottomBarWhenPushed = true
         
         if navigationController.viewControllers.count == 1 {
-            navigationController.navigationBar.isHidden = true
+            navigationController.navigationBar.isHidden = TabPage.tabItemInfos[tabBarIndex].navigationBarHidden
         } else {
             navigationController.navigationBar.isHidden = false
         }
